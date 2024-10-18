66faed734e5ea46a57a5c4862757e129bbf156e2
BeroVlad2 <glamandir1@gmail.com>
2024-10-18 16:55:01 +0300

    Lab work 3

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  {
+    ignores: ['dist', 'node_modules']
+  },
+  {
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended
+    ],
+    files: ['**/*.{js,ts}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    rules: {
+      // Тут ви можете додати будь-які специфічні правила для JavaScript
+    },
+  }
+);
